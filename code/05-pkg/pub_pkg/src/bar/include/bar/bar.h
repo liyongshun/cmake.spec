7984947b791d11e48ee2e05024d0b92c98f26313
liu.guangcong <horance@aliyun.com>
2022-07-31 07:06:00 +0800

    pub_pkg

---
+++ code/05-pkg/pub_pkg/src/bar/include/bar/bar.h
@@ -0,0 +1,14 @@
+#ifndef DA655A09_3C34_40A1_B8F9_225623BC04EA
+#define DA655A09_3C34_40A1_B8F9_225623BC04EA
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void bar(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DA655A09_3C34_40A1_B8F9_225623BC04EA */
---
+++ code/05-pkg/pub_pkg/src/foo/include/foo/foo.h
@@ -0,0 +1,14 @@
+#ifndef C813BE32_9674_45D3_9A08_275018AA873A
+#define C813BE32_9674_45D3_9A08_275018AA873A
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void foo(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* C813BE32_9674_45D3_9A08_275018AA873A */
